c7e295a04976471efb96bc5d69c5bb094ee1f8dc
Pearel Nazareth <pearelnazareth@gmail.com>
2019-10-17 15:23:37 +0530

    Added index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+ var app = express();
+
+ app.use(express.static('static'));
+ app.use(express.static('res'));
+
+ app.use(bodyParser.urlencoded({ extended: true}));
+
+ app.get('/', function(req, res){
+     res.set('Access-Control-Allow-Origin', '*');
+     res.sendFile(path.join(__dirname + '/index.html'));
+ });
+
+ app.listen(process.env.PORT);
